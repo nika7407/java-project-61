c6c16129c592e65a0ebbfeca627620224a2ab717
nika7407 <nikala7407@gmail.com>
2024-10-24 13:09:41 +0400

    renamed file and directory correctly

--- app/src/Main/java/hexlet/code/App.java
+++ app/src/Main/java/hexlet/code/App.java
@@ -2,8 +2,9 @@ package hexlet.code;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-  public static void main(String[] args){
-      System.out.println("Hello World!");
-  }
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Brain Games!");
+    }
+
 }
