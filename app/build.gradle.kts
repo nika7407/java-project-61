a1001843a8e718d4f8498abc8cfbbe75ef4dc14b
nika7407 <nikala7407@gmail.com>
2024-11-01 01:38:45 +0400

    idk at this point

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
     id("java")
-    id("application")
-    id("checkstyle")
 }
 
 group = "hexlet.code"
@@ -10,23 +8,6 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
-
-application {
-    mainClass = "hexlet.code.App"
-}
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
-
-tasks.getByName("run", JavaExec::class) {
-    standardInput = System.`in`
-}
-checkstyle {
-    toolVersion = "10.12.4"
-    configFile = file("config/checkstyle/checkstyle.xml")
-}
-
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
